e346222672b3d25be63107da4c29f3b093e5270c
marcotrf <tmarco783@gmail.com>
2022-08-22 22:50:46 -0300

    Definindo algumas entidades.

---
+++ src/FamilyTree/FamilyTree.Domain/Entities/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree.Domain.Entities
+{
+    public class Person
+    {
+        public int Identifier { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ src/FamilyTree/FamilyTree.Domain/Entities/Relationship.cs
@@ -0,0 +1,18 @@
+﻿using FamilyTree.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FamilyTree.Domain.Entities
+{
+    public class Relationship
+    {
+        public int Parent { get; set; }
+
+        public int Children { get; set; }
+
+        public RelationshipTypeEnum RelationshipType { get; set; }
+    }
+}
