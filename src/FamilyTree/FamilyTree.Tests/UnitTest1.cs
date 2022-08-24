50d9289dc9033a5e1f2e906f16cc7f6b661e6b09
marcotrf <tmarco783@gmail.com>
2022-08-24 19:49:53 -0300

    Definindo projeto de testes.

---
+++ src/FamilyTree/FamilyTree.Tests/UnitTest1.cs
@@ -0,0 +1,12 @@
+namespace FamilyTree.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
